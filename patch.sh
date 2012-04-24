36c63ab62520f1c93352250ec199bc21ee8c1ec3
Koen Kooi <koen@dominion.thruhere.net>
2012-04-24 13:50:20 +0200

    beagleboard-3.0: move to 3.0.29
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- patch.sh
+++ patch.sh
@@ -13,7 +13,7 @@ PATCHPATH="${DIR}/patches"
 EXPORTPATH="${DIR}/export"
 
 #For TAG, use mainline Kernel tags
-TAG="v3.0.28"
+TAG="v3.0.29"
 EXTRATAG=""
 
 #EXTERNAL_TREE="git://arago-project.org/git/projects/linux-am33x.git"
