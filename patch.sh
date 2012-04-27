6e340402e764c39957d69b1cb168739fd80bcdb4
Koen Kooi <koen@dominion.thruhere.net>
2012-04-27 15:39:32 +0200

    beagleboard-3.2: update to 3.2.16
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- patch.sh
+++ patch.sh
@@ -13,7 +13,7 @@ PATCHPATH="${DIR}/patches"
 EXPORTPATH="${DIR}/export"
 
 #For TAG, use mainline Kernel tags
-TAG="v3.2.15"
+TAG="v3.2.16"
 EXTRATAG=""
 
 #EXTERNAL_TREE="git://arago-project.org/git/projects/linux-am33x.git"
