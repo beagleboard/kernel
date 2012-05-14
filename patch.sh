1a869d36436d9b7f95f8f6a58b9cbd1503e34e4d
Robert Nelson <robertcnelson@gmail.com>
2012-05-14 12:06:28 -0500

    beagleboard-3.4: update to 3.4-rc7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -13,7 +13,7 @@ PATCHPATH="${DIR}/patches"
 EXPORTPATH="${DIR}/export"
 
 #For TAG, use mainline Kernel tags
-TAG="v3.4-rc5"
+TAG="v3.4-rc7"
 EXTRATAG=""
 
 #EXTERNAL_TREE="git://arago-project.org/git/projects/linux-am33x.git"
