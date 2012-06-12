9c4f99acb696589e5cdb660d878562c34babf538
Robert Nelson <robertcnelson@gmail.com>
2012-06-12 10:54:20 -0500

    beagleboard-3.2: update to 3.2.20
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -13,7 +13,7 @@ PATCHPATH="${DIR}/patches"
 EXPORTPATH="${DIR}/export"
 
 #For TAG, use mainline Kernel tags
-TAG="v3.2.19"
+TAG="v3.2.20"
 EXTRATAG=""
 
 #EXTERNAL_TREE="git://arago-project.org/git/projects/linux-am33x.git"
