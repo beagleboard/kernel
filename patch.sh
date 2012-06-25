ca65ea850b50084d79f6eeb0acbb462b61632ac6
Robert Nelson <robertcnelson@gmail.com>
2012-06-25 11:50:23 -0500

    beagleboard-3.4: update to 3.4.4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -13,7 +13,7 @@ PATCHPATH="${DIR}/patches"
 EXPORTPATH="${DIR}/export"
 
 #For TAG, use mainline Kernel tags
-TAG="v3.4.3"
+TAG="v3.4.4"
 EXTRATAG=""
 
 #EXTERNAL_TREE="git://arago-project.org/git/projects/linux-am33x.git"
