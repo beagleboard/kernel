04a279ac551b2215b952d2aa3d6d0d82dccb83ff
Koen Kooi <koen@dominion.thruhere.net>
2012-07-24 16:01:26 +0200

    beaglebone-3.6: add regulator support (breaks boot) and pinctrl support
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- patch.sh
+++ patch.sh
@@ -20,7 +20,7 @@ EXTERNAL_TREE="git://git.kernel.org/pub/scm/linux/kernel/git/tmlind/linux-omap.g
 EXTERNAL_BRANCH="master"
 EXTERNAL_SHA="55936cdfaaf11ac352b56bc58e42d6661e65ee13"
 
-PATCHSET="vaibhav usb"
+PATCHSET="vaibhav usb regulator pinctrl"
 
 git_kernel_stable () {
 	git pull git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git master --tags || true
