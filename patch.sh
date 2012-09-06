5aba67ab2d8d81ab6914a339ca5ab2e42047f690
Koen Kooi <koen@dominion.thruhere.net>
2012-09-06 15:54:19 +0200

    beaglebone 3.6: register SPI in the DT for am335x, disabled by default
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- patch.sh
+++ patch.sh
@@ -20,7 +20,7 @@ EXTERNAL_TREE="git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
 EXTERNAL_BRANCH="master"
 EXTERNAL_SHA="080909503664641432cc8adf2ee2084775fd992a"
 
-PATCHSET="devel-dt vaibhav net-next-am335x cpsw usb pinctrl rtc dma mmc da8xx-fb fixes cpufreq"
+PATCHSET="devel-dt vaibhav net-next-am335x cpsw usb pinctrl rtc dma mmc da8xx-fb fixes cpufreq spi"
 
 git_kernel_stable () {
 	git pull git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git master --tags || true
