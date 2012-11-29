162d000430d80cdb5d88588322cc365b09458a7b
Koen Kooi <koen@dominion.thruhere.net>
2012-11-29 11:19:45 +0100

    3.7: add BQL patch for smc911x
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- patch.sh
+++ patch.sh
@@ -26,7 +26,7 @@ EXTERNAL_SHA="2844a48706e54ddda4a04269dba4250b42f449de"
 
 #PATCHSET="pruss pwm2 fixes"
 # BAD: rtc
-PATCHSET="dma cpsw pinctrl cpufreq adc pwm i2c usb da8xx-fb mmc fixes f2fs 6lowpan capebus hdmi arm omap omap_sakoman omap_beagle_expansion omap_beagle omap_panda omap_sgx omap_fixes omap_thermal"
+PATCHSET="dma cpsw pinctrl cpufreq adc pwm i2c usb da8xx-fb mmc fixes f2fs 6lowpan capebus hdmi arm omap omap_sakoman omap_beagle_expansion omap_beagle omap_panda omap_sgx omap_fixes omap_thermal net"
 
 git_kernel_stable () {
 	git pull git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git master --tags || true
