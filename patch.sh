74d801d3ad89d17df6e6f722c75980e4ec162bcc
Koen Kooi <koen@dominion.thruhere.net>
2013-02-09 10:44:02 +0100

    3.8: rc7
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- patch.sh
+++ patch.sh
@@ -17,12 +17,12 @@ RECIPENAME="linux-mainline_3.8.bb"
 RECIPEFILE="${DIR}/recipes/${RECIPENAME}"
 
 #For TAG, use mainline Kernel tags
-TAG="v3.8-rc6"
+TAG="v3.8-rc7"
 EXTRATAG=""
 
 EXTERNAL_TREE="git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
 EXTERNAL_BRANCH="master"
-EXTERNAL_SHA="124b69b6cc1020fff589312c62fcaf360abd8d12"
+EXTERNAL_SHA="836dc9e3fbbab0c30aa6e664417225f5c1fb1c39"
 
 PATCHSET="dma rtc pinctrl cpufreq adc i2c da8xx-fb pwm mmc crypto 6lowpan capebus arm omap omap_sakoman omap_beagle_expansion omap_beagle omap_panda net drm not-capebus pru usb PG2 reboot iio"
 
