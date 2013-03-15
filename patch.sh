bed676cea712092733d0432d075e25700f9291c6
Koen Kooi <koen@dominion.thruhere.net>
2013-03-15 09:18:43 +0100

    3.8: update to 3.8.3
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- patch.sh
+++ patch.sh
@@ -17,12 +17,12 @@ RECIPENAME="linux-mainline_3.8.bb"
 RECIPEFILE="${DIR}/recipes/${RECIPENAME}"
 
 #For TAG, use mainline Kernel tags
-TAG="v3.8.2"
+TAG="v3.8.3"
 EXTRATAG=""
 
 EXTERNAL_TREE="git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git"
 EXTERNAL_BRANCH="linux-3.8.y"
-EXTERNAL_SHA="19b00d2dc9bedf0856e366cb7b9c7733ded659e4"
+EXTERNAL_SHA="1a45c310b2102c58e37f84abba67fe21d5d6edcf"
 
 PATCHSET="dma rtc pinctrl cpufreq adc i2c da8xx-fb pwm mmc crypto 6lowpan capebus arm omap omap_sakoman omap_beagle_expansion omap_beagle omap_panda net drm not-capebus pru usb PG2 reboot iio w1 gpmc"
 
--- recipes/linux-mainline_3.8.bb
+++ recipes/linux-mainline_3.8.bb
@@ -6,13 +6,13 @@ KERNEL_IMAGETYPE = "uImage"
 COMPATIBLE_MACHINE = "(beaglebone)"
 
 # The main PR is now using MACHINE_KERNEL_PR, for omap3 see conf/machine/include/omap3.inc
-MACHINE_KERNEL_PR_append = "c"
+MACHINE_KERNEL_PR_append = "a"
 
 FILESPATH =. "${FILE_DIRNAME}/linux-mainline-3.8:${FILE_DIRNAME}/linux-mainline-3.8/${MACHINE}:"
 
 S = "${WORKDIR}/git"
 
-PV = "3.8.2"
+PV = "3.8.3"
 
 SRC_URI = "SEDMEURI"
 SRCREV_pn-${PN} = "SEDMEREV"
