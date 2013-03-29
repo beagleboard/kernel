a79189cfc1ea7c10524d5892e3eb4f20e0d2663e
Koen Kooi <koen@dominion.thruhere.net>
2013-03-29 08:04:34 +0100

    3.8: update to 3.8.5
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- patch.sh
+++ patch.sh
@@ -17,12 +17,12 @@ RECIPENAME="linux-mainline_3.8.bb"
 RECIPEFILE="${DIR}/recipes/${RECIPENAME}"
 
 #For TAG, use mainline Kernel tags
-TAG="v3.8.4"
+TAG="v3.8.5"
 EXTRATAG=""
 
 EXTERNAL_TREE="git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git"
 EXTERNAL_BRANCH="linux-3.8.y"
-EXTERNAL_SHA="405acc3402a3df8df967d1848947dc58f0059664"
+EXTERNAL_SHA="aa4cfdeb17e3559fe1e80175c7338e065553dce5"
 
 PATCHSET="dma rtc pinctrl cpufreq adc i2c da8xx-fb pwm mmc crypto 6lowpan capebus arm omap omap_sakoman omap_beagle_expansion omap_beagle omap_panda net drm not-capebus pru usb PG2 reboot iio w1 gpmc mxt ssd130x"
 
