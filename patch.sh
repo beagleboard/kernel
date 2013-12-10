8f50cd95e0178961566fb4bd80fee8c94f1c94d6
Cody Lacey <clacey@ti.com>
2013-12-10 14:15:56 -0600

    Audio-Add Support for Analog Audio on 3.12
    These patches add analog audio support for
    the 3.12 kernel, and they modify the beagle
    defconfigs to build aic and hdmi audio
    drivers into the kernel image.
    
    Testing with Arduino Tre confirms audio
    playback through the 3.5mm output and
    record through the 3.5mm input.
    
    Platform: AM335 Arduino Tre with onboard
    TLV320AIC3104 audio codec running Angstrom
    with the Beagle Linux 3.12 Kernel.
    
    TODO: Test on BBB with audio cape and add
    audio cape DT overlay.

--- patch.sh
+++ patch.sh
@@ -24,7 +24,7 @@ EXTERNAL_TREE="git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable
 EXTERNAL_BRANCH="linux-3.12.y"
 EXTERNAL_SHA="81605d3a8b0a3af65473d7623ee1c36e8e51a0aa"
 
-PATCHSET="omap-next-dt dma-devel general-fixes dtc-fixes dtc-overlays of-fixes pdev-fixes mmc-fixes dts-fixes i2c-fixes pinctrl-fixes capemgr reset capes lcdc-fixes net deassert-hard-reset cape-import"
+PATCHSET="omap-next-dt dma-devel general-fixes dtc-fixes dtc-overlays of-fixes pdev-fixes mmc-fixes dts-fixes i2c-fixes pinctrl-fixes capemgr reset capes lcdc-fixes net deassert-hard-reset cape-import audio drm"
 
 git_kernel_stable () {
 	git pull git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git master --tags || true
