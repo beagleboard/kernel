73362ace134126a6093e185807faf727641ea1db
Robert Nelson <robertcnelson@gmail.com>
2014-01-30 09:18:08 -0600

    3.12: update to v3.12.9
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -17,12 +17,12 @@ RECIPENAME="linux-mainline_3.12.bb"
 RECIPEFILE="${DIR}/recipes/${RECIPENAME}"
 
 #For TAG, use mainline Kernel tags
-TAG="v3.12.8"
+TAG="v3.12.9"
 EXTRATAG=""
 
 EXTERNAL_TREE="git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git"
 EXTERNAL_BRANCH="linux-3.12.y"
-EXTERNAL_SHA="97f15f187a315f6a563dd5724c0cc8cde9044963"
+EXTERNAL_SHA="4a5804b8c293b23786b0743fc9c64b64f5099048"
 
 PATCHSET="omap-next-dt dma-devel general-fixes dtc-fixes dtc-overlays of-fixes pdev-fixes mmc-fixes dts-fixes i2c-fixes pinctrl-fixes capemgr reset capes lcdc-fixes net deassert-hard-reset cape-import audio drm cpufreq pru"
 
